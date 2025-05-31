e6f11b6150cb1aadcaf83f1bcf31c6aac61f4022
Alice Mikhaylenko <alicem@gnome.org>
2025-05-31 19:38:40 +0400

    Update dep URLs

--- clone-deps.sh
+++ clone-deps.sh
@@ -3,8 +3,8 @@
 git clone https://github.com/mupen64plus/mupen64plus-core
 git clone https://github.com/mupen64plus/mupen64plus-rsp-hle
 git clone https://github.com/gonetz/GLideN64
-git clone https://github.com/alice-mkh/parallel-rdp
-git clone https://github.com/alice-mkh/parallel-rsp
+git clone https://github.com/highscore-emu/parallel-rdp
+git clone https://github.com/highscore-emu/parallel-rsp
 
 pushd GLideN64
 wget https://gitlab.gnome.org/World/highscore/-/raw/main/flatpak/cores/gliden64-framebuffer-fix.patch
