1943f4b85a4963e77225b9788a33198323ed39e0
Alice Mikhaylenko <alicem@gnome.org>
2025-09-06 03:27:00 +0400

    Add a license header

--- mupen64plus-highscore.c
+++ mupen64plus-highscore.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2023-2025 Alice Mikhaylenko <alicem@gnome.org>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #include "mupen64plus-highscore.h"
 
 #include <mupen64plus/m64p_common.h>
