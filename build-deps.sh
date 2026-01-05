84f913ed4433f37b8a990cace2f9e1cfb641e2dc
Alice Mikhaylenko <alicem@gnome.org>
2026-01-05 18:59:12 +0400

    build-deps: Improve formatting

--- build-deps.sh
+++ build-deps.sh
@@ -3,19 +3,36 @@
 # A script for building Mupen64Plus-Core and plugins for hacking in toolbox
 
 pushd mupen64plus-core/projects/unix
-make all PREFIX=/usr OSD=0 NEW_DYNAREC=1 -j 4
-sudo make install PREFIX=/usr LIBDIR=/usr/lib64 OSD=0 NEW_DYNAREC=1
+make all -j 4 \
+    PREFIX=/usr \
+    OSD=0 \
+    NEW_DYNAREC=1
+sudo make install \
+    PREFIX=/usr \
+    LIBDIR=/usr/lib64 \
+    OSD=0 \
+    NEW_DYNAREC=1
 popd
 
 pushd mupen64plus-rsp-hle/projects/unix
-make all APIDIR=/usr/include/mupen64plus -j 4
-sudo make install PREFIX=/usr LIBDIR=/usr/lib64 APIDIR=/usr/include/mupen64plus
+make all -j 4 \
+    APIDIR=/usr/include/mupen64plus
+sudo make install \
+    PREFIX=/usr \
+    LIBDIR=/usr/lib64 \
+    APIDIR=/usr/include/mupen64plus
 popd
 
 pushd GLideN64/src
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_LIBDIR=lib64 -DMUPENPLUSAPI=ON -DNO_OSD=ON -DNOHQ=ON
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_INSTALL_LIBDIR=lib64 \
+    -DMUPENPLUSAPI=ON \
+    -DNO_OSD=ON \
+    -DNOHQ=ON
 cmake --build . --parallel
 sudo cmake --install .
 popd
@@ -23,7 +40,9 @@ popd
 pushd parallel-rsp
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --parallel
 sudo cmake --install .
 popd
